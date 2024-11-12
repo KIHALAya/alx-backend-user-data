a1c6c2cba92ff809998bdaa3c192042a5257598b
KIHALAya <kihalaya00@gmail.com>
2024-11-12 22:28:07 +0000

    implementing basic authentication process

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python3
+"""Authentication module for the API.
 """
-Authentication module
-"""
-from flask import request
+import re
 from typing import List, TypeVar
+from flask import request
 
 
 class Auth:
-    """Template class for authentication system """
-
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Method to determine if authentication is required """
-        if path is None:
-            return True
-        if not excluded_paths or len(excluded_paths) == 0:
-            return True
-
-        if path[-1] != '/':
-            path += '/'
-
-        for excluded_path in excluded_paths:
-            if path == excluded_path:
-                return False
-
+        """Checks if a path requires authentication.
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get the authorization header """
+        """Gets the authorization header field from the request.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Method to get the current user"""
+        """Gets the current user from the request.
+        """
         return None
